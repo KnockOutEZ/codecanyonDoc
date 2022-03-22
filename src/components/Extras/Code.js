6f4cc78e937f8ee6042c358825946789a643cb9e
KnockOutEZ <70368615+KnockOutEZ@users.noreply.github.com>
2022-03-22 17:48:48 +0600

    code block done

--- src/components/Extras/Code.js
+++ src/components/Extras/Code.js
@@ -1,5 +1,32 @@
-const Code = () => {
-    return (  );
-}
- 
+import { CopyBlock, dracula } from "react-code-blocks";
+
+const Code = ({
+  text,
+  language,
+  showLineNumbers,
+  startingLineNumber,
+  theme,
+}) => {
+  return (
+    <CopyBlock
+      text={text}
+      language={language}
+      showLineNumbers={showLineNumbers}
+      startingLineNumber={startingLineNumber}
+      theme={dracula}
+      wrapLines
+    />
+  );
+};
+
 export default Code;
+
+
+// Usage
+// import Code from "./Extras/Code";
+{/* <Code
+        text={"My code snippet goes here \n qweqwe"}
+        language={"js"}
+        showLineNumbers={false}
+        startingLineNumber={1}
+      ></Code> */}
